98b71bd913d5c7bdad011c9d18a80201991651db
Andreas Bauer <andreas.bauer@h-its.org>
2015-09-01 10:35:36 +0200

    next try to get readthedocs working

--- setup.py
+++ setup.py
@@ -1,26 +1,34 @@
 from setuptools import setup, Extension
 
+#try:
+#	import numpy
+#except ImportError:
+#	raise Exception("numpy is requiered")
+
 try:
 	import numpy
-except ImportError:
-	raise Exception("numpy is requiered")
+	
+	npy_include_dir = numpy.get_include()
 
-npy_include_dir = numpy.get_include()
-
-incl_dirs = [npy_include_dir,]
-libs = ['m']
+	incl_dirs = [npy_include_dir,]
+	libs = ['m']
 
 
-calcGrid = Extension(   'gadget.calcGrid',
+	calcGrid = Extension(   'gadget.calcGrid',
             include_dirs = incl_dirs,
             libraries    = libs,
             sources = ['libs/calcGrid.c','libs/sph.c', 'libs/dg.c'])
 
+	ext_modules = [calcGrid,]
+except Exception as inst:
+	print(inst)
+	print("can not build c module")
+	ext_modules = []
 
 setup(name='gadget',
       version='0.1',
       packages=['gadget', 'arepo'],
-      ext_modules = [calcGrid,],
+      ext_modules = ext_modules,
       scripts = ['scripts/arepoinfo', 'scripts/arepoconvert']
       )
 
