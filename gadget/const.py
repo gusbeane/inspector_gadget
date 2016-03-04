f4d070439706fd14eaa51f0a137a7f12e11c4b2b
Andreas Bauer <andreas.bauer@h-its.org>
2016-03-04 14:58:14 +0100

    documentation bug fix

--- gadget/const.py
+++ gadget/const.py
@@ -6,12 +6,17 @@ import numbers
 
 import gadget.units as u
 
-
-
-
-c = speedoflight = u.Quantity(299792458., unit=u.meter/u.second)
-pc = parsec = u.Quantity(1., unit=u.parsec)
-kpc = kiloparsec = u.Quantity(1., unit=u.kiloparsec)
-mpc = megaparsec = u.Quantity(1., unit=u.megaparsec)
-AU = astronomicalunit = u.Quantity(1., unit=u.astronomicalunit)
-H = Hubble = u.Quantity(100., unit=u.kms/u.mpc*u.h)
+try:
+    
+    c = speedoflight = u.Quantity(299792458., unit=u.meter/u.second)
+    pc = parsec = u.Quantity(1., unit=u.parsec)
+    kpc = kiloparsec = u.Quantity(1., unit=u.kiloparsec)
+    mpc = megaparsec = u.Quantity(1., unit=u.megaparsec)
+    AU = astronomicalunit = u.Quantity(1., unit=u.astronomicalunit)
+    H = Hubble = u.Quantity(100., unit=u.kms/u.mpc*u.h)
+    
+    
+    
+    
+except:
+    print("Could not setup constants")
